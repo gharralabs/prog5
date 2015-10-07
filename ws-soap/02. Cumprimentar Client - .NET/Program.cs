1e035185f63263bd69882592662f03ce0677adb7
Marine King <mkp@prime.co.kr>
2015-10-07 08:44:59 -0300

    Cumprimentar Cliente - .NET

---
+++ ws-soap/02. Cumprimentar Client - .NET/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02.Cumprimentar_Client__.NET.br.gharralabs.services;
+
+namespace _02.Cumprimentar_Client__.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloServiceImplClient cliente;
+            cliente = new HelloServiceImplClient();
+
+            String nome, resposta;
+
+            Console.WriteLine("Informe o seu nome:");
+            nome = Console.ReadLine();
+
+            resposta = cliente.cumprimentar(nome);
+            Console.WriteLine(resposta);
+            Console.ReadKey();
+        }
+    }
+}
